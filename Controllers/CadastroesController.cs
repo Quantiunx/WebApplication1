55fb6f5de5519f265aed0a1fd9a4eb259ece9044
Filipe Miorin de Lima <filipe.mlima@outlook.com.br>
2022-08-01 10:59:56 -0300

    Initial commit

---
+++ Controllers/CadastroesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CadastroesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CadastroesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cadastroes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cadastro.ToListAsync());
+        }
+
+        // GET: Cadastroes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cadastro = await _context.Cadastro
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cadastro == null)
+            {
+                return NotFound();
+            }
+
+            return View(cadastro);
+        }
+
+        // GET: Cadastroes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cadastroes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Sobrenome,Cidade,Estado")] Cadastro cadastro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cadastro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cadastro);
+        }
+
+        // GET: Cadastroes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cadastro = await _context.Cadastro.FindAsync(id);
+            if (cadastro == null)
+            {
+                return NotFound();
+            }
+            return View(cadastro);
+        }
+
+        // POST: Cadastroes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Sobrenome,Cidade,Estado")] Cadastro cadastro)
+        {
+            if (id != cadastro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cadastro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CadastroExists(cadastro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cadastro);
+        }
+
+        // GET: Cadastroes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cadastro = await _context.Cadastro
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cadastro == null)
+            {
+                return NotFound();
+            }
+
+            return View(cadastro);
+        }
+
+        // POST: Cadastroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cadastro = await _context.Cadastro.FindAsync(id);
+            _context.Cadastro.Remove(cadastro);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CadastroExists(int id)
+        {
+            return _context.Cadastro.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Cadastro.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Cadastro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
